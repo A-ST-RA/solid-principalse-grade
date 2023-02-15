c476118df87fa8c71d82e34a12fa50b135e67ea5
Vladislav Bachmatskiy <v.bakhmatsky@webpractik.ru>
2023-02-15 15:17:22 +0300

    feat(srp): writed example of srp

---
+++ SingleResponsibility/bad.ts
@@ -0,0 +1,14 @@
+// Bad
+export class EpicriseProcessor {
+  readEpicrise() {
+    console.log("reading epicrise from disk...");
+  }
+
+  validateEpicrise() {
+    console.log("validating epicrise...");
+  }
+  
+  extractEpicriseInfo() {
+    console.log("extracting epicrise info..."); 
+  }
+}
---
+++ SingleResponsibility/good.ts
@@ -0,0 +1,18 @@
+// Good
+export class EpicriseValidator {
+  validateEpicrise() {
+    console.log("validating epicrise...");
+  }
+}
+
+export class FileReader {
+  readEpicrise() {
+    console.log("reading epicrise from disk...");
+  }
+}
+
+export class EpicriseExtractor {
+  extractEpicriseInfo() {
+    console.log("extracting epicrise info..."); 
+  }
+}
